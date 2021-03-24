ea22c20afd8e94a266a3d17190c440d8004ee364
Wentian Xia <xiawentian1998@outlook.com>
2021-03-24 17:31:17 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp zsh/zsh.config ~/.zshrc
+cp zsh/p10k.cinfig ~/.p10k.zsh
+source ~/.zshrc
+source ~/.p10k.zsh
+
+cp vim/vimrc.config ~/.vimrc
+mkdir -p ~/.vim/colors/
+cp vim/colors/jellybeans.vim ~/.vim/colors/jellybeans.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+cd ~/.vim/plugged/YouCompleteMe
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install mono-complete golang nodejs default-jdk npm
+./install.py --all
+
